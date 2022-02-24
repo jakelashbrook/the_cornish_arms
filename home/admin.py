a2a63f430603a87f39ce0ee5dc4364d9c2b0fbf2
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-02-24 20:15:45 +0000

    home models added and registered to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import OpeningTime, FoodTime, Event
 # Register your models here.
+
+
+admin.site.register(OpeningTime)
+admin.site.register(FoodTime)
+admin.site.register(Event)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class OpeningTime(models.Model):
+    ''' Set the Opening Times Model '''
+    class Meta:
+        verbose_name_plural = ' Opening Times'
+
+
+    weekday_opening = models.CharField(max_length=10, null=False, blank=False)
+    weekday_closing = models.CharField(max_length=10, null=False, blank=False)
+    weekend_opening = models.CharField(max_length=10, null=False, blank=False)
+    weekend_closing = models.CharField(max_length=10, null=False, blank=False)
+
+class FoodTime(models.Model):
+    ''' Food service hours '''
+    class Meta:
+        verbose_name_plural = ' Food Times'
+
+
+    lunch_start = models.CharField(max_length=10, null=False, blank=False)
+    lunch_end = models.CharField(max_length=10, null=False, blank=False)
+    dinner_start = models.CharField(max_length=10, null=False, blank=False)
+    dinner_end = models.CharField(max_length=10, null=False, blank=False)
+
+class Event(models.Model):
+    ''' To publish events being held '''
+    class Meta:
+        verbose_name_plural = ' Events'
+
+
+    event_name = models.CharField(max_length=20, null=True, blank=True)
+    event_day = models.CharField(max_length=20, null=True, blank=True)
+    event_time = models.CharField(max_length=20, null=True, blank=True)
+    event_info = models.TextField(max_length=300, null=True, blank=True)
+    event_image = models.ImageField(default=0)
